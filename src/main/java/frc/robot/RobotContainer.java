8971fc25339c3314115375c021f628c94a9337a7
Ved Atal <ved1atal1@gmail.com>
2023-01-13 22:56:16 -0800

    one constructor version

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -86,9 +86,9 @@ public class RobotContainer {
         //     s_Swerve.getPose(), 
         //     s_Swerve.getPose().plus(new Transform2d(new Translation2d(-0.5, 0), new Rotation2d()))));
         smartPathing.onTrue(new OnTheFlyGeneration(
-            // new Pose2d(new Translation2d(0, 1), new Rotation2d(0)),
-            s_Swerve.getPose(), 
-            4));
+            new Pose2d(new Translation2d(0, 1), new Rotation2d(0)),
+            s_Swerve.getPose() 
+            ));
     }
 
     /**
