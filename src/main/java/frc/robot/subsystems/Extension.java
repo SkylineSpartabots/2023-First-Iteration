1f4cada79b4a5df7b7549f4eccbc8d99a09fdeb2
Atharv Jain <atharvjain05@gmail.com>
2023-01-28 14:03:32 -0800

    Change States to Single Solenoid, Formatted Main and Robot
    Other small changes too!

--- src/main/java/frc/robot/subsystems/Extension.java
+++ src/main/java/frc/robot/subsystems/Extension.java
@@ -4,13 +4,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Extension extends SubsystemBase {
     static Extension instance;
