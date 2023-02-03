5fed25f9e12f119beaddf13a268170b22d47136e
VincentShao32 <yunbo2016@gmail.com>
2023-02-02 19:41:19 -0800

    elevator subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,9 +18,9 @@ public final class Constants {
     public static final double stickDeadband = 0.1;
 
     public static final class HardwarePorts {
-        public static final int pivotMasterMotor = 20; //i set master and follower arbitrarily
-        public static final int pivotFollowerMotor = 45; //arbitrary
-        public static final int extensionMotor = 21; //this one is a neo someone needs to get out the neo hardware tuning thing
+        public static final int elevatorLeaderMotor = 20; //i set master and follower arbitrarily
+        public static final int elevatorFollowerMotor = 45; //arbitrary
+        public static final int armnMotor = 21; //its a falcon now :)
         public static final int intakeMotor = 32; //correct
         public static final int intakeSolenoidForwardChannel = 0;
         public static final int intakeSolenoidReverseChannel = 0;
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
     static ElevatorSubsystem instance;
@@ -8,7 +13,71 @@ public class ElevatorSubsystem extends SubsystemBase {
         if (instance == null) instance = new ElevatorSubsystem();
         return instance;
     }
+    private TalonFX mLeaderElevatorMotor, mFollowerElevatorMotor;
+    private double velocity;
+    ElevatorStates elevatorState = ElevatorStates.ZERO;
+
+    public enum ElevatorStates {
+		ZERO(0.0),
+		GROUND(0.0),
+		SUBSTATION(0.0),
+		L1(0.0),
+		L2(0.0),
+		L3(0.0),
+		TEST(1.0); // for testing
+
+		double statePosition = 0.0;
+
+		private ElevatorStates(double statePosition) {
+			this.statePosition = statePosition;
+		}
+	}
+    
     public ElevatorSubsystem() {
-        
+        mLeaderElevatorMotor = new TalonFX(Constants.HardwarePorts.elevatorLeaderMotor);
+        configureMotor(mLeaderElevatorMotor, false);
+        mFollowerElevatorMotor = new TalonFX(Constants.HardwarePorts.elevatorFollowerMotor, "2976 CANivore");
+        configureMotor(mFollowerElevatorMotor, false);
+        mFollowerElevatorMotor.set(ControlMode.Follower, Constants.HardwarePorts.elevatorLeaderMotor);
+    }
+
+    private void configureMotor(TalonFX talon, boolean b){
+        talon.setInverted(b);
+        talon.configVoltageCompSaturation(12.0, Constants.timeOutMs);
+        talon.enableVoltageCompensation(true);
+        talon.setNeutralMode(NeutralMode.Brake);
+        talon.config_kF(0, 0, Constants.timeOutMs);
+        talon.config_kP(0, 0, Constants.timeOutMs);
+        talon.config_kI(0, 0, Constants.timeOutMs);
+        talon.config_kD(0, 0, Constants.timeOutMs);
+    }
+
+    public setVelocity(double velocity) {
+        this.velocity = velocity;
+        mLeaderElevatorMotor.set(ControlMode.Velocity, velocity);
+    }
+    public setPosition(ElevatorStates state) {
+        elevatorState = state;
+        mLeaderElevatorMotor.set(ControlMode.Position, state);
+    }
+    public getVelocitySetpoint() {
+        return velocity;
+    }
+    public getPositionSetpoint() {
+        return elevatorState.statePosition;
+    }
+    public double getMeasuredPosition () {
+		return mLeaderElevatorMotor.getSelectedSensorPosition();
+	}
+
+	public void setEncoderPosition (double position) {
+		mLeaderElevatorMotor.setSelectedSensorPosition(position);
+	}
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("elevator pos setpoint", getPositionSetpoint());
+		SmartDashboard.putNumber("elevator pos measured", getMeasuredPosition());
+		SmartDashboard.putNumber("elevator set velo", getVelocitySetpoint());
     }
 }
