9f403ab132c8292d5980699a8f50a517f3af43a2
Ved Atal <ved1atal1@gmail.com>
2023-03-01 16:52:00 -0800

    auto dock testing

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -11,30 +11,63 @@ public class AutoBalance extends CommandBase {
     Swerve s_Swerve;
     double robotPitch;
     double driveSpeed;
-    PIDController driveController = new PIDController(-0.01, 0, 0);  // tune PID
+    PIDController driveController = new PIDController(-0.015, 0, 0); // tune PID
     int counter = 0;
+    double direction = 1.0;
+    boolean finished = false;
+
     public AutoBalance() {
         s_Swerve = Swerve.getInstance();
         addRequirements(s_Swerve);
     }
 
     @Override
+    public void initialize() {
+        direction = Math.copySign(direction, robotPitch);
+    }
+
+    @Override
     public void execute() {
         counter++;
-        if (counter % 5 == 0) {
-        robotPitch = s_Swerve.getPitch();
-        if (Math.abs(robotPitch) < 0.3) {
-            robotPitch = 0;
+        if (counter % 20 == 0) {
+            if(Math.abs(s_Swerve.getPitch())+3 >= Math.abs(robotPitch)) {
+                driveSpeed = driveController.calculate(s_Swerve.getPitch(), 0);
+                SmartDashboard.putNumber("drive speed", driveSpeed);
+                SmartDashboard.putNumber("last pit", Math.abs(robotPitch));
+                SmartDashboard.putNumber("curr pit 3", Math.abs(s_Swerve.getPitch())+3);
+                s_Swerve.drive(
+                    new Translation2d(driveSpeed, 0).times(Constants.SwerveConstants.maxSpeed),
+                    0,
+                    true,
+                    true);
+            } else {
+                finished = true;
+            }
+            robotPitch = s_Swerve.getPitch();
         }
-        driveSpeed = driveController.calculate(robotPitch, 0);
-        SmartDashboard.putNumber("drive speed", driveSpeed);
+        
+        // driveSpeed = driveController.calculate(robotPitch, 0);
+        // SmartDashboard.putNumber("drive speed", driveSpeed);
+        // s_Swerve.drive(
+        // new Translation2d(driveSpeed, 0).times(Constants.SwerveConstants.maxSpeed),
+        // 0,
+        // true,
+        // true);
+        // }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
         s_Swerve.drive(
-            new Translation2d(driveSpeed, 0).times(Constants.SwerveConstants.maxSpeed), 
-            0, 
-            true, 
-            true
-        );
+                new Translation2d(0, 0).times(Constants.SwerveConstants.maxSpeed),
+                0,
+                true,
+                true);
     }
-    }
-    
+
 }
